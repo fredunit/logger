c4f6b2b2024b619e4d2adfa5932129bebf141de3
ho lun chow <fredunit2007@gmail.com>
2021-07-06 13:40:19 +0800

    fix response time

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 // app.js
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-const routes = express.Router()
-const myLogger = require('./logger')
-
-app.use(myLogger)
-app.use(routes)
-app.use(express.urlencoded({ extended: true }))
+const port = 3000
+const fs = require('fs')
 
 
-app.get('/', async (req, res) => {
-  res.send('Hello World!')
-  res.status(201).end()
+app.use((req, res, next) => {
+  const beginn = new Date()
+  next()
+  const end = new Date()
+  const diff = (beginn - end).toLocaleString()
+  const log = `${beginn} | ${req.method} from ${req.originalUrl} | total time: ${diff}`
+  console.log(log)
+  next()
+  fs.appendFile('serverLog.txt', log + "\n", err => {
+    if (err) {
+      console.log(err)
+    }
+  })
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
 })
 
 app.get('/new', (req, res) => {
   res.send('新增 Todo 頁面')
-  res.status(201).end()
 })
  
 app.get('/:id', (req, res) => {
   res.send('顯示一筆 Todo')
-  res.status(201).end()
 })
 
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
-  res.status(201).end()
 })
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
 })
-
